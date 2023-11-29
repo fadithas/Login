bf2ca48595282906449924a4a8a67df01b8a755b
fadithas <thasneem.manthiri@gmail.com>
2023-11-29 10:32:30 +0100

    created login page

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const express =require("express");
+const cors=require("cors");
 const PORT=3000;
 const app=express();
+app.use(cors());
+app.use(express.json());
 app.get("/",(req,res)=>{
-    res.send('<h1>HELLO WORLD</h1>')
+    res.send({
+        data:employees,
+        message:"success"
+    })
+
+    
 })
-app.listen (3000,()=>{
+app.post("/login",(req,res)=>{
+    console.log({body:req.body});
+    const {email,password}=req.body;
+    const rightEmail="admin@admin.com";
+    const rightPassword="12345";
+    if (email===rightEmail){if(password===rightPassword){
+   res.send(
+        {
+            message:"Login Success!!!",
+            user:{
+                name:"Abdullah",
+                role:"Admin",
+                employee_code:3214
+            }
+        })
+    
+     
+}else{
+        res.status(401).send({
+            message:"Wrong Password"
+        })
+    }
+
+} else {
+    res.status(401).send({
+        message:"Account does not Exist"
+    })
+}
+
+})  
+    
+
+
+app.listen (PORT,()=>{
     console.log("The server is running at port 3000");
-});
+})
 
 
